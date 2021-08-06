28a2c506f85e84beabf5f96f8571e78930974ccc
iosdev <ioswap.service@gmail.com>
2021-08-06 15:48:44 +0800

    okt

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  OKT = 66,
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x65728c1d0e545db117940d5745089c256516ad43'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x2b090dd2252bb5f19bacc4b509dcc9f64c49b151028561918ec29cf409a7093e'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
